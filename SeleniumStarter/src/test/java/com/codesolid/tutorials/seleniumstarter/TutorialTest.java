6255ce4a443b0c003e73a7d4d0348acc01dc19b3
JohnLockwood <elitepropertiesbroker@gmail.com>
2014-06-16 06:51:03 -0700

    SeleniumStarter Initial Checkin

---
+++ SeleniumStarter/src/test/java/com/codesolid/tutorials/seleniumstarter/TutorialTest.java
@@ -0,0 +1,76 @@
+package com.codesolid.tutorials.seleniumstarter;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TutorialTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://github.com";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTutorial() throws Exception {
+    driver.get(baseUrl + "/CodeSolid");
+    driver.findElement(By.linkText("tutorials")).click();
+    String text = driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div[2]/div[7]/article/h1")).getText();
+    assertTrue(text != null);
+    assertTrue(text.contains("Welcome"));
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
